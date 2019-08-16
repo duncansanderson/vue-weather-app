ea411603a8ea5778c6ad3e6992dc8faa16f54b5c
Duncan Sanderson <duncan@duncansanderson.co.uk>
2019-08-16 19:14:12 +0100

    FEAT (Express) Force https

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const http = require('http')
+const enforce = require('express-sslify')
 const path = require('path')
 const serveStatic = require('serve-static')
 
 let app = express()
 app.use(serveStatic(__dirname + '/dist'))
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
