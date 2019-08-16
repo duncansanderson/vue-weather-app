dc55c6667432462bebb2d4b21ee783403744499b
Duncan Sanderson <duncan@duncansanderson.co.uk>
2019-08-16 16:43:22 +0100

    TEST - remove css

--- src/App.vue
+++ src/App.vue
@@ -11,80 +11,80 @@
 </template>
 
 <script>
-import CurrentWeather from './components/CurrentWeather'
-import Loader from './components/Loader'
-import Location from './components/Location'
-import TemperatureChart from './components/TemperatureChart'
-import { mapState } from 'vuex'
+// import CurrentWeather from './components/CurrentWeather'
+// import Loader from './components/Loader'
+// import Location from './components/Location'
+// import TemperatureChart from './components/TemperatureChart'
+// import { mapState } from 'vuex'
 
-export default {
-	name: 'app',
-  	components: {
-        CurrentWeather,
-        Loader,
-        Location,
-        TemperatureChart
-    },
-    computed: {
-        ...mapState({
-            locationReceived: state => state.locationReceived,
-            weatherReceived: state => state.weatherReceived
-        })
-    },
-}
+// export default {
+// 	name: 'app',
+//   	components: {
+//         CurrentWeather,
+//         Loader,
+//         Location,
+//         TemperatureChart
+//     },
+//     computed: {
+//         ...mapState({
+//             locationReceived: state => state.locationReceived,
+//             weatherReceived: state => state.weatherReceived
+//         })
+//     },
+// }
 </script>
 
 <style lang="scss">
-* {
-	box-sizing: border-box;
-	font-family: $roboto;
-	font-weight: 100;
-	// font-size: 3.25vh;
-}
+// * {
+// 	box-sizing: border-box;
+// 	font-family: $roboto;
+// 	font-weight: 100;
+// 	// font-size: 3.25vh;
+// }
 
-body {
-	align-items: center;
-	background: #e7e7ef;
-	display: flex;
-	justify-content: center;
-}
+// body {
+// 	align-items: center;
+// 	background: #e7e7ef;
+// 	display: flex;
+// 	justify-content: center;
+// }
 
-html, body {
-	font-size: 1rem;
-	height: 100%;
-	margin: 0;
-	padding: 0;
-	width: 100%;
-}
+// html, body {
+// 	font-size: 1rem;
+// 	height: 100%;
+// 	margin: 0;
+// 	padding: 0;
+// 	width: 100%;
+// }
 
-#app {
-    // align-items: center;
-	background-color: $blue;
-	box-shadow: 0 0 4vh rgba(0, 0, 0, 0.1);
-    color: $white;
-    // display: flex;
-    // flex-direction: column;
-	height: 100vh;
-  	overflow: hidden;
-    width: 100vw;
+// #app {
+//     // align-items: center;
+// 	background-color: $blue;
+// 	box-shadow: 0 0 4vh rgba(0, 0, 0, 0.1);
+//     color: $white;
+//     // display: flex;
+//     // flex-direction: column;
+// 	height: 100vh;
+//   	overflow: hidden;
+//     width: 100vw;
     
-    display: grid;
-    // grid-template-rows: repeat(3, 1fr);
-    grid-template-rows: 25% 1fr 200px;
-    position: relative;
+//     display: grid;
+//     // grid-template-rows: repeat(3, 1fr);
+//     grid-template-rows: 25% 1fr 200px;
+//     position: relative;
   
-  	@media (min-width: 576px) {
-    	height: 580px;
-    	width: 320px;
-      }
+//   	@media (min-width: 576px) {
+//     	height: 580px;
+//     	width: 320px;
+//       }
       
-      .chart-wrapper {
-          height: 200px;
+//       .chart-wrapper {
+//           height: 200px;
 
-          canvas {
-              height: 200px !important;
-              width: 100% !important;
-          }
-      }
-}
+//           canvas {
+//               height: 200px !important;
+//               width: 100% !important;
+//           }
+//       }
+// }
 </style>
