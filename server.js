c33f834187e118e2365236c8001d18eb1ff703c5
Duncan Sanderson <duncan@duncansanderson.co.uk>
2019-08-18 15:36:20 +0100

    FEAT (Express) Force https

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const http = require('http')
-const enforce = require('express-sslify')
-const path = require('path')
-const serveStatic = require('serve-static')
+// const express = require('express')
+// const http = require('http')
+// const enforce = require('express-sslify')
+// const path = require('path')
+// const serveStatic = require('serve-static')
 
-let app = express()
-app.use(serveStatic(__dirname + '/dist'))
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+// let app = express()
+// app.use(serveStatic(__dirname + '/dist'))
+// app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
-const port = process.env.PORT || 5000
-http.createServer(app).listen(port, () => {
-    console.log('Express server listening on port ' + port)
-})
+// const port = process.env.PORT || 5000
+// http.createServer(app).listen(port, () => {
+//     console.log('Express server listening on port ' + port)
+// })
+
+const express = require('express');
+const http = require('http');
+const enforce = require('express-sslify');
+ 
+const app = express();
+ 
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+ 
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
